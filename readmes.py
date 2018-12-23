fdd64481312915c987abb1b6923b953499776b54
Brady Trainor <mail@bradyt.com>
2018-12-23 12:38:26 -0800

    Add username to filename to avoid possible conflicts

--- readmes.py
+++ readmes.py
@@ -1,11 +1,44 @@
-
+import os.path
 import subprocess
 import urllib.request
 
-url = 'https://raw.githubusercontent.com/Malabarba/smart-mode-line/master/README.org'
+urls = [
+    "https://raw.githubusercontent.com/Malabarba/smart-mode-line/master/README.org",
+    "https://raw.githubusercontent.com/bbatsov/projectile/master/README.md",
+    "https://raw.githubusercontent.com/purcell/exec-path-from-shell/master/README.md",
+    "https://raw.githubusercontent.com/wasamasa/dotemacs/master/init.org",
+]
 
-response = urllib.request.urlopen(url)
-data = response.read()
-text = data.decode('utf-8')
 
-subprocess.run(["pandoc", text, "-o", "README.org", "--columns=70"])
+def construct_filename(url):
+    u = url.split("/")
+    name = u[3]
+    repo = u[4]
+    filename = u[6]
+    return "-".join((name, repo, filename))
+
+
+def ensure_download(url, filename):
+    if not os.path.isfile(filename):
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        text = data.decode("utf-8")
+        with open(os.path.join("responses", filename), "w") as f:
+            f.write(text)
+
+
+def replace_extension(filename):
+    return os.path.splitext(filename)[0] + ".org"
+
+
+def ensure_conversion(filename):
+    target = os.path.join("readmes", replace_extension(filename))
+    if not os.path.isfile(target):
+        download = os.path.join("responses", filename)
+        subprocess.run(["pandoc", download, "--output", target, "--columns=70"])
+
+
+for url in urls:
+    filename = construct_filename(url)
+    ensure_download(url, filename)
+    ensure_conversion(filename)
